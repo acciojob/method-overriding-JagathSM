4ed10006db89a04ccfdfe9c9b835cd2358a1a913
Jagath <jagathsingularity@gmail.com>
2022-12-12 22:15:36 +0530

    method overriding assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,31 @@
 package com.driver;
 
 public class Main {
+
+    public class A {
+
+        // Task 1:
+        public String meth(){
+            return "Invoking method from class A";
+        }
+
+    }
+
+
+    public class B extends A {
+
+        // Task 4:
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+
     public static void main(String[] args) {
 
         // Task 3:
-        B b = new B();
+        Main.B b = new Main().new B();
 
         System.out.println(b.meth()); // Output if called this method
         // before overriding method in A, Output: Invoking method from class A
